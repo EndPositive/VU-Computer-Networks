39e5c17e0c49f29fa9ca040872c42fc7c47455d7
EndPositive <jop-zitman@hotmail.com>
2020-02-09 20:52:23 +0100

    Make dem private and use Wait(int) instead of Reveived

--- 3.1 Chat Client/client.py
+++ 3.1 Chat Client/client.py
@@ -1,37 +1,40 @@
 import socket
 import threading
 
+
 class ChatClient:
     def __init__(self):
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.__socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        self.pushThread = threading.Thread(target=self.push)
-        self.pushThread.setDaemon(True)
+        self.__pushThread = threading.Thread(target=self.__push)
+        self.__pushThread.setDaemon(True)
 
-        self.pullThread = threading.Thread(target=self.pull)
-        self.pullThread.setDaemon(True)
+        self.__pullThread = threading.Thread(target=self.__pull)
+        self.__pullThread.setDaemon(True)
 
         self.Quit = False
-        self.Received = False
+        self.__Wait = 0
+
+        self.name = ""
 
     def start(self):
-        self.socket.connect(('18.195.107.195', 5378))
-        if self.connect():
-            self.pushThread.start()
-            self.pullThread.start()
+        self.__socket.connect(('18.195.107.195', 5378))
+        if self.__connect():
+            self.__pushThread.start()
+            self.__pullThread.start()
         else:
             self.close()
 
-    def send(self, msg):
+    def __send(self, msg):
         try:
-            self.socket.sendall(msg.encode('utf-8'))
+            self.__socket.sendall(msg.encode('utf-8'))
             return True
         except socket.error:
             return False
 
-    def receive(self, size):
+    def __receive(self, size):
         try:
-            data = self.socket.recv(size)
+            data = self.__socket.recv(size)
             if not data:
                 return False
             else:
@@ -39,63 +42,63 @@ class ChatClient:
         except socket.error:
             return False
 
-    def connect(self):
+    def __connect(self):
         print("Username:")
         name = input()
         if name:
-            if self.send('HELLO-FROM ' + name + '\n'):
-                res = self.receive(4096)
+            if self.__send('HELLO-FROM ' + name + '\n'):
+                res = self.__receive(4096)
                 if res:
                     spl = res.split()
                     if spl[0] == "IN-USE":
                         print("Username already in use.")
-                        return self.connect()
+                        return self.__connect()
                     elif spl[0] == "BUSY":
                         print("Server is busy.")
                     elif spl[0] == "HELLO":
+                        self.name = name
                         print("Connected.")
                         return True
                 else:
                     print("Something went wrong.")
         else:
-            return self.connect()
+            return self.__connect()
         print("Disconnecting from host...")
         return False
 
-    def push(self):
-        while True:
-            print("\nCommand:")
-            inp = input()
-            if inp:
-                spl = inp.split()
-                if spl[0] == "!quit":
-                    self.Quit = True
-                elif spl[0] == "!who":
-                    if not self.send('WHO\n'):
-                        print("Something went wrong.\nDisconnecting from host...")
+    def __push(self):
+        while True and not self.Quit:
+            if self.__Wait == 0:
+                print("\nCommand:")
+                inp = input()
+                if inp:
+                    spl = inp.split()
+                    if spl[0] == "!quit":
                         self.Quit = True
-                elif inp[0] == "@":
-                    user = spl[0][1:]
-                    msg = " ".join(spl[1:])
-                    if not self.send("SEND " + user + " " + msg + "\n"):
-                        print("Something went wrong.\nDisconnecting from host...")
-                        self.Quit = True
-                else:
-                    self.Received = True
-                    print("Unknown command")
+                    elif spl[0] == "!who":
+                        self.__Wait = 1
+                        if not self.__send('WHO\n'):
+                            print("Something went wrong.\nDisconnecting from host...")
+                            self.Quit = True
+                    elif inp[0] == "@":
+                        user = spl[0][1:]
+                        msg = " ".join(spl[1:])
+                        if user == "echobot" or user == self.name:
+                            self.__Wait = 2
+                        else:
+                            self.__Wait = 1
+                        if not self.__send("SEND " + user + " " + msg + "\n"):
+                            print("Something went wrong.\nDisconnecting from host...")
+                            self.Quit = True
+                    else:
+                        print("Unknown command")
 
-                if self.Quit:
-                    break
-
-                while True and not self.Received:
-                    pass
-                self.Received = False
-
-    def pull(self):
+    def __pull(self):
         while True:
-            res = self.receive(4096)
+            res = self.__receive(4096)
             if res:
                 spl = res.split()
+                print('\x1b[1A' + '\x1b[2K' + '\x1b[1A')
                 if spl[0] == "WHO-OK":
                     print("Online users: ", ",".join(spl[1:]))
                 elif spl[0] == "SEND-OK":
@@ -103,7 +106,6 @@ class ChatClient:
                 elif spl[0] == "UNKNOWN":
                     print("User is not online.")
                 elif spl[0] == "DELIVERY":
-                    print('\x1b[1A' + '\x1b[2K' + '\x1b[1A')
                     print("Received msg from " + spl[1] + ": ", " ".join(spl[2:]))
                 elif spl[0] == "BAD-RQST-HDR":
                     print("Unknown command.")
@@ -112,18 +114,15 @@ class ChatClient:
                 else:
                     print("Unknown error")
 
-                if not spl[0] == "DELIVERY":
-                    self.Received = True
+                self.__Wait -= 1
             else:
                 print("Something went wrong, disconnected from host.")
                 self.Quit = True
 
-            if self.Quit:
-                break
-
     def close(self, code=0):
-        self.socket.close()
+        self.__socket.close()
         exit(code)
+
 
 if __name__ == '__main__':
     chatClient = ChatClient()
