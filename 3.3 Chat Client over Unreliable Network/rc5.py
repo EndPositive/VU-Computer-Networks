c42f2118045fc276e36a45f00b79ca91389d6768
EndPositive <jop-zitman@hotmail.com>
2020-02-23 17:02:57 +0100

    RC5

---
+++ 3.3 Chat Client over Unreliable Network/rc5.py
@@ -0,0 +1,92 @@
+import math
+
+
+def rotate_left(a, n, w=16):
+    for i in range(n):
+        c = a & (1 << (w - 1))
+        a <<= 1
+        if c:
+            a |= 1
+        a &= (pow(2, w) - 1)
+    return a
+
+
+def rotate_right(a, n, w=16):
+    for i in range(n):
+        c = a & 1
+        a >>= 1
+        if c:
+            a |= (1 << (w - 1))
+        a &= (pow(2, w) - 1)
+    return a
+
+
+class rc5:
+    def __init__(self):
+        # The key, considered as an array of bytes
+        self.K = [0x91, 0x5F, 0x46, 0x19, 0xBE, 0x41, 0xB2, 0x51, 0x63, 0x55, 0xA5, 0x01, 0x10, 0xA9, 0xCE, 0x91]
+
+        # The number of rounds to use when encrypting data.
+        self.r = 2
+
+    def get_key(self):
+        # The length of the key in words (or 1, if b=0)
+        c = math.ceil(max(len(self.K), 1) / 2)
+        # A temporary working array used during key scheduling. initialized to the key in words.
+        L = [0] * c
+        # The number of round subkeys required.
+        t = 2 * (self.r + 1)
+        # The round subkey words.
+        S = [0] * t
+
+        # L key in words
+        for i in range(c - 1, -1, -1):
+            L[i] = self.K[i * 2] + (self.K[i * 2 - 1] << 8)
+
+        # S subkey array
+        S[0] = 0xB7E1
+        for i in range(1, t):
+            S[i] = S[i - 1] + 0x9E37
+
+        i = j = A = B = 0
+        for x in range(3 * max(t, c)):
+            A = S[i] = rotate_left(S[i] + A + B, 3)
+            B = L[j] = rotate_left(L[j] + A + B, A + B)
+            i = (i + 1) % t
+            j = (j + 1) % c
+
+        return S
+
+    def encrypt(self, A, B, S):
+        print(A, B)
+        A = A + S[0]
+        B = B + S[1]
+        for i in range(1, self.r + 1):
+            A = rotate_left(A ^ B, B) + S[2 * i]
+            B = rotate_left(B ^ A, A) + S[2 * i + 1]
+            print(A, B)
+
+        return A, B
+
+    def decrypt(self, A, B, S):
+        print(A, B)
+        for i in range(self.r, 0, -1):
+            B = rotate_right(B - S[2 * i + 1], A) ^ A
+            A = rotate_right(A - S[2 * i], B) ^ B
+            print(A, B)
+
+        B = B - S[1]
+        A = A - S[0]
+        print(A, B)
+
+        return A, B
+
+
+if __name__ == "__main__":
+    rc5 = rc5()
+    key = rc5.get_key()
+    A, B = rc5.encrypt("h".encode()[0], "e".encode()[0], key)
+    print("")
+    A, B = rc5.decrypt(A, B, key)
+    print(chr(A), chr(B))
+
