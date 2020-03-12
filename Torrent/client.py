71ff5e275b3250adac0fa6974ecf2820fb8c4d5b
EndPositive <jop-zitman@hotmail.com>
2020-03-12 22:32:51 +0100

    xvids

--- Torrent/client.py
+++ Torrent/client.py
@@ -1,5 +1,6 @@
 import socket
 import time
+import threading
 from packet import *
 from util import *
 
@@ -11,7 +12,14 @@ class Client:
     def start(self):
         # Connect to bootstrap
         self.__socket.connect(('80.112.140.14', 65400))
-        self.__push()
+        pullThread = threading.Thread(target=self.__pull)
+        pullThread.setDaemon(True)
+        pullThread.start()
+        pushThread = threading.Thread(target=self.__push)
+        pushThread.setDaemon(True)
+        pushThread.start()
+        while True:
+            pass
 
     def __push(self):
         while True:
@@ -34,23 +42,26 @@ class Client:
                 packet.type = 6
                 # packet.hash = " ".split(inp)[1]
                 packet.piece_no = " ".split(inp)[2]
+            elif "punch" in inp:
+                packet.type = 6
+                by = packet.to_bytes()
+                self.__socket.connect((" ".split(inp)[1], int(" ".split(inp)[2])))
             else:
                 print("unknown command")
                 return
 
             by = packet.to_bytes()
             send(self.__socket, by)
-            time.sleep(0.3)
+            time.sleep(0.2)
+
+    def __pull(self):
+        while True:
             res = receive(self.__socket, 4096)
             if not res:
                 print("Something bad happend: ", res)
                 return
             p = Packet(res)
-            if p.type == packet.type:
-                print("Success")
-            else:
-                print("Something bad happend, type: ", p.type, p.err)
-
+            print(p.type)
 
 
 if __name__ == "__main__":
