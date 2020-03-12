200349e9c849099dd367cd66d26a9179953b0ac7
EndPositive <jop-zitman@hotmail.com>
2020-03-12 17:23:01 +0100

    Util

---
+++ Torrent/util.py
@@ -0,0 +1,24 @@
+import socket
+
+
+def send(conn, msg):
+    try:
+        conn.sendall(msg.encode('utf-8'))
+        return True
+    except socket.error:
+        return False
+
+
+def receive(conn, size):
+    try:
+        data = conn.recv(size)
+        if not data:
+            return False
+        else:
+            while not data[-1:] == b'\n':
+                data += conn.recv(size)
+                if not data:
+                    break
+            return data.decode("utf-8")
+    except socket.error:
+        return False
