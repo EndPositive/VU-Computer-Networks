92b92b23e7151512cdeb557c23260079f243f34e
Tiberiu Iancu <tiberiu.iancu@gmail.com>
2020-03-12 17:59:30 +0100

    packet

---
+++ Torrent/packet.py
@@ -0,0 +1,61 @@
+class MalformedFrameError(Exception):
+    def __init__(self, expression=None, message=None):
+        self.expression = expression
+        self.message = message
+
+class Packet:
+    def __init__(self, data=None):
+        if data is None:
+            self.type = None
+            self.hash = None
+            self.seeders = None
+            self.err = None
+            self.piece_no = None
+            self.data = None
+            return
+
+        if len(data) < 17:
+            raise MalformedFrameError()
+
+        self.type = data[0]
+        self.hash = data[1:17]
+
+        index = 17
+        # parse request list of seeders
+        if self.type == 3:
+            self.seeders = []
+            while len(data) > index + 6:
+                self.seeders.append((data[index: index + 4], data[index + 5: index + 6]))
+        elif self.type == 5:
+            # TODO: ERR CODES (MAYBE SKIP)
+            self.err = int.from_bytes(data[index:], 'big')
+        elif self.type == 6:
+            if len(data) < index + 4:
+                raise MalformedFrameError()
+            self.piece_no = int.from_bytes(data[index: index + 4], 'big')
+        elif self.type == 7:
+            if len(data) < index + 4:
+                raise MalformedFrameError()
+            self.piece_no = int.from_bytes(data[index: index + 4], 'big')
+            if len(self.data) == 0:
+                raise MalformedFrameError()
+            self.data = data[index + 4:]
+
+    def to_bytes(self):
+        data = b''
+        data += self.type.to_bytes(1, 'big')
+        data += self.hash
+
+        if self.type == 3:
+            for seeder in self.seeders:
+                data += seeder[0]
+                data += seeder[1]
+        elif self.type == 5:
+            data += self.err
+        elif self.type == 6:
+            data += self.piece_no.to_bytes(4, 'big')
+        elif self.type == 7:
+            data += self.piece_no.to_bytes(4, 'big')
+            data += self.data
+
+        return data
