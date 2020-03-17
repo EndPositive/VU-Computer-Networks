bf13750e164b3737a4fb22288a0fbc4823a476b8
Tiberiu Iancu <tiberiu.iancu@gmail.com>
2020-03-17 21:51:20 +0100

    fixed for you

--- Torrent/torrent.py
+++ Torrent/torrent.py
@@ -2,7 +2,7 @@ import pickle
 from file_manager import File
 from hashlib import md5
 from threading import Lock
-from os.path import basename, splitext
+from os.path import basename, splitext, getsize
 
 mutex = Lock()
 
@@ -24,6 +24,8 @@ class Torrent:
             self.pieces = _pieces
 
         self.server = _server
+        if self.file_size is None:
+            self.file_size = getsize(self.file.path)
         self.file_size = _file_size
 
         self.__curr_piece = 0
