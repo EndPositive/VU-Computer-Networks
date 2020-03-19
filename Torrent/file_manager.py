d2bcfe0aba72b8ac40eabae980c0a0ed0227cdd1
tiberiuiancu <tiberiu.iancu@gmail.com>
2020-03-19 12:07:44 +0100

    print for debugging later

--- Torrent/file_manager.py
+++ Torrent/file_manager.py
@@ -31,6 +31,7 @@ class File:
         return self.f.read(self.piece_size)
 
     def write_piece(self, piece_number, data):
+        print('Writing', str(len(data)), 'btyes to location', piece_number * self.piece_size)
         self.f.seek(piece_number * self.piece_size)
         return self.f.write(data)
 
